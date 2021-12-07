ba847e5966f26afda26854baa3038289748bd158
eGangotri <egangotritrust@gmail.com>
2021-12-08 03:34:50 +0800

    emurgo

--- permutation.ts
+++ permutation.ts
@@ -1,7 +1,7 @@
 const input = '101*011*1';
-let permutationSet:Array<any> = [];
+let permutationSet:Array<any> = [[]];
 
-const evaluate = (str:string, permutationSet :Array<any>) => {
+const evaluate = (str:string, permutationSet :Array<any>):Array<any> => {
   if (str.length === 0) { return permutationSet ; }
   
   if (str[0] === '0' || str[0] === '1') {
@@ -12,16 +12,21 @@ const evaluate = (str:string, permutationSet :Array<any>) => {
 
   if (str[0] === '*') {
     let len = permutationSet.length;
+
     for (let i = 0; i < len; i++) {
       let temp = permutationSet [i].slice(0);
       permutationSet .push(temp);
     }
-    for (let i = 0; i < permutationSet .length; i++) {
-      (i < permutationSet .length/2) ? permutationSet [i].push('0') : permutationSet [i].push('1');  
+    for (let i = 0; i < permutationSet.length; i++) {
+      (i < permutationSet.length/2) ? permutationSet [i].push('0') : permutationSet [i].push('1');  
     }
   }
-  console.log(permutationSet)
+  return evaluate(str.substring(1), permutationSet);
 }
 
-evaluate(input,permutationSet)
-console.log(`Result for permutation of ${input} is ${permutationSet}`);
+evaluate(input,permutationSet);
+console.log(`Result for permutation of ${input} is`);
+permutationSet.forEach((x)=>{
+    console.log(x.join(""))
+})
+
